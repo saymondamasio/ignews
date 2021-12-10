54f8cc33ef0ae3564805094dffe9d94f741acfb5
Saymon Damásio <saymon.damasio95@gmail.com>
2021-12-10 17:48:21 -0300

    feat: page home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,27 @@
 import Head from 'next/head'
+import Image from 'next/image'
+
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <div>
-        <h1>Ola</h1>
-      </div>
+      
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+            <span>👏 Hey, welcome </span>
+            <h1>News about the <span>React</span> world.</h1>
+            <p>
+              Get access to all the publications <br />
+              <span>for $9.90 month</span>
+            </p>
+        </section>
+
+        <Image src="/images/avatar.svg" alt="Girl coding" width={336} height={521}/>
+      </main>
     </>
   )
 }
