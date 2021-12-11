d31119cc364e33e536c6b0bb6268c92b28bb0d28
Saymon Damásio <saymon.damasio95@gmail.com>
2021-12-11 17:23:33 -0300

    feat: add user in faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
+import { query } from "faunadb"
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { fauna } from "../../../services/fauna"
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -15,4 +17,44 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user, account, profile }){
+      const { email } = user
+
+      try {
+        // FQL
+        //Cria um novo usuário com email
+        await fauna.query(
+          //se o usuário não existe, cria um novo usuário, senão retorna o usuário
+          query.If(
+            // não existe o usuário com o email
+            query.Not(
+              query.Exists(
+                query.Match(
+                  query.Index('user_by_email'),
+                  query.Casefold(email!)
+                )
+              )
+            ),
+            // cria um usuario
+            query.Create(query.Collection('users'), { data: { email } }),
+            // retorna o usuário
+            query.Get(
+              query.Match(
+                  query.Index('user_by_email'),
+                  query.Casefold(email!)
+                )
+            )
+          )
+        )
+        
+        return true
+      } catch (error) {
+        console.log('Error in FaunaDB: ' + error); 
+
+        return false
+      }
+      
+    }
+  }
 })
