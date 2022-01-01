9529be902eb4576622cd47fef45df6a7986081b5
Saymon Damásio <saymon.damasio95@gmail.com>
2022-01-01 18:27:52 -0300

    feat: testing Header component

---
+++ src/components/Header/Header.spec.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react'
+import { Header } from '.'
+
+jest.mock('next/router', () => ({
+  useRouter: () => ({
+    asPath: '/'
+  })
+}))
+
+jest.mock('next-auth/react', () => ({
+  useSession: () => ({
+    data: {}
+  })
+}))
+
+describe('Header component', () => {
+  it('renders correctly', () => {
+    render(
+      <Header />
+    )
+  
+    expect(screen.getByText('Home'))
+    expect(screen.getByText('Posts'))
+  })
+})
