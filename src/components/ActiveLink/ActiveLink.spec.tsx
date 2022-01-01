7bddcb235caaccfd7f404d02f40d21368ce9590e
Saymon Damásio <saymon.damasio95@gmail.com>
2022-01-01 17:54:05 -0300

    feat: testing ActiveLink component

---
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -0,0 +1,30 @@
+import { render } from '@testing-library/react'
+import { ActiveLink } from '.'
+
+jest.mock('next/router', () => ({
+  useRouter: () => ({
+    asPath: '/'
+  })
+}))
+
+describe('ActiveLink component', () => {
+  it('renders correctly', () => {
+    const { getByText } = render(
+      <ActiveLink href='/' activeClassName='active'>
+        <a>Home</a>
+      </ActiveLink>
+    )
+  
+    expect(getByText('Home')).toBeInTheDocument()
+  })
+  
+  it('add an active class if the route is the same as the link', () => {
+    const { getByText } = render(
+      <ActiveLink href='/' activeClassName='active'>
+        <a>Home</a>
+      </ActiveLink>
+    )
+  
+    expect(getByText('Home')).toHaveClass('active')
+  })
+})
