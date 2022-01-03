6a1e2036d09069cf9610e7395923f45af938c06f
Saymon Damásio <saymon.damasio95@gmail.com>
2022-01-03 12:12:40 -0300

    feat: add secret next auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -17,6 +17,7 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  secret: process.env.NEXT_AUTH_SECRET,
   callbacks: {
     async session({ session }) {
       if (!session.user?.email) {
